98754276d7b3666c0127b3f508d4b61fc0ff2cfc
Fikry1 <fikry.muhammad@hotmail.com>
2022-08-15 05:54:04 +0200

    django import_export

--- searchdb/admin.py
+++ searchdb/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Student,Student2
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Student2)
+# admin.site.register(Student)
+# admin.site.register(Student2)
+
+@admin.register(Student)
+class StudentImportExport(ImportExportModelAdmin):
+    pass
+
+@admin.register(Student2)
+class Studen2tImportExport(ImportExportModelAdmin):
+    pass
+
